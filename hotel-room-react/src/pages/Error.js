060bb9b2abf84df7f2001ea1f0289b71c8cf5dae
Postimal <michal.postek1@wp.pl>
2020-01-02 16:23:51 +0100

    navbar finish, banner finish, services finish

--- hotel-room-react/src/pages/Error.js
+++ hotel-room-react/src/pages/Error.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Hero from "../components/Hero";
+import { Link } from 'react-router-dom';
+import Banner from "../components/Banner";
+
 
 const Error = () => {
-  return <div>hello from error page</div>;
+  return <Hero>
+     <Banner title='404' subtitle='page not found'>
+       <Link to='/' className='btn-primary'>
+         return home
+       </Link>
+     </Banner>
+  </Hero>
 };
 
 export default Error;
--- hotel-room-react/src/pages/Rooms.js
+++ hotel-room-react/src/pages/Rooms.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Hero from "../components/Hero";
+import { Link } from 'react-router-dom';
+import Banner from "../components/Banner";
+
 
 const Rooms = () => {
-  return <div>hello from rooms page</div>;
+  return <Hero hero="roomsHero">
+    <Banner title='our rooms'>
+      <Link to='/' className='btn-primary'>back home</Link>
+  </Banner>
+  </Hero>
 };
 
 export default Rooms;

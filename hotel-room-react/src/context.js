eeaa7d380db249da252b652d4fcb466e0829515a
Postimal <michal.postek1@wp.pl>
2020-01-06 15:12:25 +0100

    add and finish roomFilter,RoomList components

--- hotel-room-react/src/context.js
+++ hotel-room-react/src/context.js
@@ -9,6 +9,15 @@ class RoomProvider extends Component {
         sortedRooms:[],
         featuredRooms:[],
         loading: true,
+        type:'all',
+        capacity:1,
+        price:0,
+        minPrice:0,
+        maxPrice:0,
+        minSize:0,
+        maxSize:0,
+        breakfast:false,
+        pets:false,
     };
     // getData
 
@@ -17,7 +26,17 @@ class RoomProvider extends Component {
         let rooms = this.formatData(items);
         // console.log(rooms);
         let featuredRooms = rooms.filter(room => room.featured);
-        this.setState({rooms, featuredRooms,sortedRooms: rooms, loading: false})
+        let maxPrice = Math.max(...rooms.map(room=>room.price));
+        let maxSize = Math.max(...rooms.map(room=>room.size));
+        this.setState({
+            rooms, 
+            featuredRooms,
+            sortedRooms: rooms, 
+            loading: false,
+            price: maxPrice,
+            maxPrice,
+            maxSize
+        })
     }
 
     formatData(items){
@@ -35,13 +54,64 @@ class RoomProvider extends Component {
         const room = tempRooms.find((room) => room.slug === slug)
         return room
     }
+
+    handleChange = (event) => {
+        const target= event.target;
+        const value = event.type ==='checkbox' ?   //dodalismy warunek dlatego ze jest checkbox. z lewej strony jest rozbite na czesci pierwsze nizej w setState w calosci
+        target.checked: target.value;
+
+        this.setState({
+            [event.target.name]: target.type ==='checkbox'? event.target.checked : event.target.value
+        }, this.filterRooms)
+        // wywolujemy filterRooms jako callback funckji setState
+    }
+
+    filterRooms = () => {
+        let {rooms, type, capacity, price, minSize, maxSize, breakfast, pets} = this.state
+        console.log('filterin at the moment')
+        // all the rooms
+        let tempRooms = [...rooms];
+        // transform values
+        capacity = parseInt(capacity);
+        price = parseInt(price);
+        // filter by type
+        if(type !== 'all'){
+            tempRooms = tempRooms.filter(room => room.type === type)   // warunek dany aby moc wyswietlic 'all' bez niego nie mozna było tego uzyskac
+        }
+
+        // filter by capacity
+        if(capacity !==1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
+        }
+
+        // filter by price
+        tempRooms = tempRooms.filter(room => room.price <= price);
+
+        // filter by size
+        tempRooms = tempRooms.filter(room => room.size >=minSize && room.size <= maxSize);
+
+        // filter by breakfast
+        if(breakfast) {
+            tempRooms = tempRooms.filter( room => room.breakfast)
+        }
+
+        // filter by pets
+        if(pets) {
+            tempRooms = tempRooms.filter( room => room.pets)
+        }
+
+        // setting state
+        this.setState({sortedRooms: tempRooms})
+    }
     
+
     render() {
         return (
            <RoomContext.Provider 
              value={{
                 ...this.state,
-                getRoom: this.getRoom
+                getRoom: this.getRoom,
+                handleChange: this.handleChange
             }}>
                {this.props.children}
             </RoomContext.Provider>
@@ -50,5 +120,6 @@ class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
 
 export { RoomProvider, RoomConsumer, RoomContext };
--- hotel-room-react/src/pages/Rooms.js
+++ hotel-room-react/src/pages/Rooms.js
@@ -2,14 +2,20 @@ import React from "react";
 import Hero from "../components/Hero";
 import { Link } from 'react-router-dom';
 import Banner from "../components/Banner";
+import RoomContainer from "../components/RoomContainer";
 
 
 const Rooms = () => {
-  return <Hero hero="roomsHero">
-    <Banner title='our rooms'>
-      <Link to='/' className='btn-primary'>back home</Link>
-  </Banner>
-  </Hero>
+  return (
+  <React.Fragment>
+    <Hero hero="roomsHero">
+      <Banner title='our rooms'>
+        <Link to='/' className='btn-primary'>back home</Link>
+      </Banner>
+    </Hero>
+    <RoomContainer />
+  </React.Fragment>
+  )
 };
 
 export default Rooms;
